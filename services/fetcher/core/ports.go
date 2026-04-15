390712b922ff8fa13adf53c9334c548a4f597cf3
fanoxiz <newfanoxiz@gmail.com>
2026-04-15 14:31:18 +0300

    refactor(fetcher): structure improve, fetcher complete

--- services/fetcher/core/ports.go
+++ services/fetcher/core/ports.go
@@ -1,9 +1,12 @@
-// services/fetcher/core/ports.go
-
 package core
+
+import "github.com/fanoxiz/crypto-monitor/services/contracts" // allowed core dependency
 
 type Exchange interface {
 	GetName() string
 	GetPrice(symbol string) (float64, error)
 }
 
+type SenderToAnalyzer interface {
+	Send(msg contracts.TickerMessage) error
+}
--- services/fetcher/main.go
+++ services/fetcher/main.go
@@ -1,95 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"math"
 	"net/http"
-	"sync"
 	"time"
 
-	"github.com/fanoxiz/crypto-monitor/services/fetcher/adapters"
+	"github.com/fanoxiz/crypto-monitor/services/fetcher/adapters/api"
+	"github.com/fanoxiz/crypto-monitor/services/fetcher/adapters/sender"
 	"github.com/fanoxiz/crypto-monitor/services/fetcher/core"
 )
 
-var reqFrequency = 500 * time.Millisecond
-
-// type StringFloat64Map struct {
-//   m sync.Map
-// }
-
-// func (s *StringFloat64Map) Store(key string, val float64) {
-//   s.m.Store(key, val)
-// }
-
-// func (s *StringFloat64Map) Load(key string) (float64, bool) {
-// 	v, ok := s.m.Load(key)
-// 	if ok {
-// 		return v.(float64), true
-// 	}
-// 	return 0, false
-// }
-
 func main() {
+	reqFrequency := 2 * time.Second
+	trackedCoins := []string{"BTC", "ETH"}
+	urlToSend := "http://localhost:8081/api/v1/prices"
+
 	openedClient := &http.Client{
 		Transport: &http.Transport{
 			MaxIdleConns:        100,
 			MaxIdleConnsPerHost: 100,
 			IdleConnTimeout:     90 * time.Second,
 		},
-		Timeout: 1 * time.Second,
+		Timeout: 2 * time.Second,
 	}
 
 	exchanges := []core.Exchange{
-		adapters.NewBinanceAdapter(openedClient),
-		adapters.NewBybitAdapter(openedClient),
-		adapters.NewCoinbaseAdapter(openedClient),
+		api.NewBinanceAdapter(openedClient),
+		api.NewBybitAdapter(openedClient),
+		api.NewCoinbaseAdapter(openedClient),
 		// adapters.NewKrakenAdapter(openedClient), doesn't work in Russia
-		adapters.NewOKXAdapter(openedClient),
+		api.NewOKXAdapter(openedClient),
 	}
 
-	trackedCoins := []string{"BTC", "ETH"}
+	senderAdapter := sender.NewSenderService(openedClient, urlToSend)
+	fetcher := core.NewFetcherService(exchanges, senderAdapter)
 
-	log.Println("Starting fetcher service...")
-
-	var wg sync.WaitGroup
-	for {
-		wg.Add(len(trackedCoins))
-		for _, coin := range trackedCoins {
-			go func() {
-				price, exchange := trackBestPrice(coin, exchanges)
-				fmt.Printf("[%s] [$%.2f \t\t(%s)]\n", coin, price, exchange)
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-		fmt.Printf("------------\n")
-		time.Sleep(reqFrequency)
-	}
-}
-
-func trackBestPrice(coinName string, exchanges []core.Exchange) (float64, string) {
-	var wg sync.WaitGroup
-
-	var mu sync.Mutex
-	var minToBuy = math.MaxFloat64
-	var bestExchange = "-"
-
-	for _, ex := range exchanges {
-		wg.Go(func() { // since Go 1.25
-			price, err := ex.GetPrice(coinName)
-			if err != nil {
-				log.Printf("Ошибка на %s: %v", ex.GetName(), err)
-				return
-			}
-			mu.Lock()
-			defer mu.Unlock()
-			if price < minToBuy {
-				minToBuy = price
-				bestExchange = ex.GetName()
-			}
-		})
-	}
-	wg.Wait()
-	return minToBuy, bestExchange
+	log.Println("Fetcher service is running...")
+	fetcher.Start(trackedCoins, reqFrequency)
 }
